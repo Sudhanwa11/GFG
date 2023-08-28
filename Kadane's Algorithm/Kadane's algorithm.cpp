9f261c70145d6327d2d8060d0e8d242388b20b0f
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-08-28 15:32:44 +0530

    Update Kadane's algorithm.cpp

--- Kadane's Algorithm/Kadane's algorithm.cpp
+++ Kadane's Algorithm/Kadane's algorithm.cpp
@@ -1,7 +1,6 @@
 class solution {
     public: 
     long long maxSubarraySum (int arr[], int n) {
-        
         long long mx = arr[0];
         long long currSum = 0;
         for (int i=0; i<n; i++) {
